d920d80cf81c4bac334ce51cf183884b8d66664d
Graham Losee <graham.losee@loblaw.ca>
2016-03-27 17:05:14 -0400

    Add about section; simplfy JS into closure.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,33 @@
-// jQuery to collapse the navbar on scroll
-function collapseNavbar() {
-		if ($(".navbar").offset().top > 50) {
-				$(".navbar-fixed-top").addClass("top-nav-collapse");
-		} else {
-				$(".navbar-fixed-top").removeClass("top-nav-collapse");
-		}
-}
-
-$(window).scroll(collapseNavbar);
-$(document).ready(collapseNavbar);
-
-// jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-		$('a.page-scroll').bind('click', function(event) {
+
+	function Navbar() {
+
+		// jQuery to collapse the navbar on scroll
+		function collapseNavbar() {
+				if ($(".navbar").offset().top > 50) {
+						$(".navbar-fixed-top").addClass("top-nav-collapse");
+				} else {
+						$(".navbar-fixed-top").removeClass("top-nav-collapse");
+				}
+		}
+
+		$(window).scroll(collapseNavbar);
+		$(document).ready(collapseNavbar);
+	}
+
+	function PageScroll() {
+		$('a.page-scroll').on('click', function(event) {
 				var $anchor = $(event.currentTarget),
-						selector = '[name=' + $anchor.attr('href').replace('#', '') + ']';
+						href = $anchor.attr('href').replace('#', ''),
+						selector = '[name=' + href + ']';
 				$('html, body').stop().animate({
 						scrollTop: $(selector).offset().top
 				}, 1000, 'easeInOutExpo');
 				event.preventDefault();
 		});
+	}
+
+	Navbar();
+	PageScroll();
 });
+// })(window, document, $);
